0beda00144a2d44cfb9af3ea7a377adcf7731471
xcr <itxcr@protonmail.com>
2021-09-05 23:30:16 +0800

    1

--- webstorm.config.js
+++ webstorm.config.js
@@ -2,19 +2,19 @@
 const path = require('path')
 
 function resolve (dir) {
-    return path.join(__dirname, dir)
+  return path.join(__dirname, dir)
 }
 
 module.exports = {
-    context: path.resolve(__dirname, './'),
-    resolve: {
-        alias: {
-            '@': resolve('src/renderer'),
-            '~': resolve('src'),
-            'root': resolve('./'),
-            '#': resolve('src/universal'),
-            'apis': resolve('src/main/apis'),
-            '@core': resolve('src/main/apis/core')
-        }
-    },
+  context: path.resolve(__dirname, './'),
+  resolve: {
+    alias: {
+      '@': resolve('src/renderer'),
+      '~': resolve('src'),
+      'root': resolve('./'),
+      '#': resolve('src/universal'),
+      'apis': resolve('src/main/apis'),
+      '@core': resolve('src/main/apis/core')
+    }
+  }
 }
